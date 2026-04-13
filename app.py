de0e233e788654c1ac9bb2f05b4446cee043c1ec
Divyanshu Kumawat <147680325+Devinkmwt01@users.noreply.github.com>
2026-04-13 18:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from src.hybrid_waf.routes.main import main_bp
 from src.hybrid_waf.routes.proxy import proxy_bp  # Import proxy Blueprint
@@ -7,9 +8,6 @@ app = Flask(__name__)
 # Register blueprints
 app.register_blueprint(main_bp)
 app.register_blueprint(proxy_bp)
-
-if __name__ == '__main__':
-    import os
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
